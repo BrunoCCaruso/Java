b4badd2d643dde369213fe7af9be9deabde88f29
Bruno Cesar Caruso <brunoccaruso.s@gmail.com>
2022-01-27 15:35:57 -0300

    Ejercicios de ejemplo POO

--- examplePOO/src/examplepoo/Persona.java
+++ examplePOO/src/examplepoo/Persona.java
@@ -6,7 +6,30 @@ public class Persona {
     private String nombre;
     private int edad;
     
-    public 
+    public Persona(){
+        
+    }
+    
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    public void establecerNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public void establecerEdad(int edad){
+        this.edad = edad;
+    }
+    
+    public int obtenerEdad(){
+        return edad;
+    }
 }
 
 
--- examplePOO/src/examplepoo/Principal.java
+++ examplePOO/src/examplepoo/Principal.java
@@ -7,21 +7,19 @@ public class Principal {
     public static void main(String[] args) {
      //Declarando una variable de tipo persona; 
         Persona persona1 = new Persona();
-        Persona persona2 = new Persona();
+        Persona persona2 = new Persona("Cristian", 20);
       
-        persona1.nombre = "Lorena";
-        persona1.edad = 25;
-        
-        persona2.nombre = "Cristian";
-        persona2.edad = 20;
+        persona1.establecerNombre("Lorena");
+        persona1.establecerEdad(25);
+          
         
         System.out.println("PERSONA 1");
-        System.out.println(persona1.nombre);
-        System.out.println(persona1.edad);
+        System.out.println(persona1.obtenerNombre());
+        System.out.println(persona1.obtenerEdad());
         
         System.out.println("PERSONA 2");
-        System.out.println(persona2.nombre);
-        System.out.println(persona2.edad);
+        System.out.println(persona2.obtenerNombre());
+        System.out.println(persona2.obtenerEdad());
         
     }
     
